e2641842a3465a1a7e8396d36263dbb82b43555b
Randi Ratnayake <randi.ratnayake@rkeytech.io>
2022-06-29 09:32:55 +1000

    Build Break

--- src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Tables/Contacts.sql
+++ src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Tables/Contacts.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL IDENTITY,
 	[FirstName] NVARCHAR(255) NULL,
 	[LastName] NVARCHAR(255) NOT NULL,
-	[IsActive] BIT NOT NULL,
+	--[IsActive] BIT NOT NULL,
 	[DepartmentId] INT,
     CONSTRAINT [PK_Contacts] PRIMARY KEY ([Id]), 
     CONSTRAINT [FK_Contacts_Deparments] FOREIGN KEY ([DepartmentId]) REFERENCES [RocketCorp].[Departments]([Id])
--- src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Views/EmployeeMasterView.sql
+++ src/RocketCorp.SqlDb.DevOps.SqlServerDataTools/Views/EmployeeMasterView.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[EmployeeMasterView]
 AS
-SELECT C.FirstName, C.LastName, D.[Name] AS DepartmentName
+SELECT C.FirstName, C.LastName, D.[Name] AS [Department Name], CASE WHEN IsActive = 1 THEN 'Yes' ELSE 'No' END AS Active
 FROM [RocketCorp].[Contacts] C INNER JOIN 
 [RocketCorp].[Departments] D ON
 C.DepartmentId = D.Id
