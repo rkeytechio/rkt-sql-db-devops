83280cca02938979eeb9cd29f59ea490ab21a11f
Randi Ratnayake <randi.ratnayake@rkeytech.io>
2022-06-29 11:33:51 +1000

    Changes to View

--- src/RocketCorp.SqlDb.DevOps.DbUp/Scripts/Views/EmployeeMasterView.sql
+++ src/RocketCorp.SqlDb.DevOps.DbUp/Scripts/Views/EmployeeMasterView.sql
@@ -5,7 +5,7 @@ GO
 
 CREATE OR ALTER VIEW [RocketCorp].[EmployeeMasterView]
 AS
-SELECT C.FirstName, C.LastName, D.[Name] AS DepartmentName
+SELECT C.FirstName, C.LastName, D.[Name] AS [Department Name], CASE WHEN IsActive = 1 THEN 'Y' ELSE 'N' END AS Active
 FROM [RocketCorp].[Contacts] C INNER JOIN 
 [RocketCorp].[Departments] D ON
 C.DepartmentId = D.Id
